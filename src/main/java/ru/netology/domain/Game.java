15aa70b901f04f1029c8bcd128a8941807d0d8c7
Азат Ильдарханов <defcon797@gmail.com>
2022-06-18 00:21:14 +0300

    Homework-15 task 2. Changed interface to HashMap

--- src/main/java/ru/netology/domain/Game.java
+++ src/main/java/ru/netology/domain/Game.java
@@ -1,52 +1,51 @@
 package ru.netology.domain;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class Game {
 
-    private Collection<Player> players;
+    private HashMap<String, Player> players;
 
     public Game() {
-        this.players = new ArrayList<>();
+        this.players = new LinkedHashMap<>();
     }
 
     //добавление игрока на турнир
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player);
     }
 
     //получить список игроков
     public Collection<Player> findAll() {
-        return players;
+        return new ArrayList<>(players.values());
     }
 
-    public Player findByName(String playerName) {
-        for (Player player : players) {
-            if (player.getName() == playerName) {
-                return player;
-            }
-        }
-        return null;
-    }
+    //public Player findByName(String playerName) {
+    //    for (Player player : players) {
+    //        if (player.getName() == playerName) {
+    //            return player;
+    //        }
+    //    }
+    //     return null;
+    //}
 
     public int round(String playerName1, String playerName2) {
         //проверка, зарегистрирован ли игрок?
-        if (findByName(playerName1) == null && findByName(playerName2) == null) {
+        if (!players.containsKey(playerName1) && !players.containsKey(playerName2)) {
             throw new NotRegisteredException("Players " + playerName1 + ", " + playerName2 + " not registered");
         }
-        if (findByName(playerName1) == null) {
+        if (!players.containsKey(playerName1)) {
             throw new NotRegisteredException("Player name " + playerName1 + " not registered");
         }
-        if (findByName(playerName2) == null) {
+        if (!players.containsKey(playerName2)) {
             throw new NotRegisteredException("Player name " + playerName2 + " not registered");
         }
 
         //вычисление победителя
-        if (findByName(playerName1).getStrength() - findByName(playerName2).getStrength() == 0) {
+        if (players.get(playerName1).getStrength() - players.get(playerName2).getStrength() == 0) {
             return 0;
         }
-        if (findByName(playerName1).getStrength() - findByName(playerName2).getStrength() > 0) {
+        if (players.get(playerName1).getStrength() - players.get(playerName2).getStrength() > 0) {
             return 1;
         } else {
             return 2;
